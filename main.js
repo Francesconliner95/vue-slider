2728f9022e10822b29e8e13f482176f5e9944a7c
Francesconliner <francescodivincenzo95@outlook.it>
2020-11-09 16:53:19 +0100

    3 bonus

--- main.js
+++ main.js
@@ -4,30 +4,39 @@ var app = new Vue({
         testo: 'Ciao a tutti!',
         immagini: ["https://c4.wallpaperflare.com/wallpaper/703/272/886/landscape-mist-hill-wallpaper-preview.jpg",
         "https://c4.wallpaperflare.com/wallpaper/622/0/6/lake-landscape-mountains-valley-wallpaper-preview.jpg",
-        "https://www.pixelstalk.net/wp-content/uploads/2016/04/Landscape-Wallpaper-download-free.jpg",
+        "https://sp-images.summitpost.org/130603.jpg?auto=format&fit=max&h=800&ixlib=php-2.1.1&q=35&s=a22fafa48e320a05164f152c6effdeba",
         "https://c4.wallpaperflare.com/wallpaper/695/352/975/usa-landscape-panorama-sunset-mountains-hd-wallpaper-preview.jpg"],
         index: 0,
+        numero: 0,
         },
     methods: {
-        next_f(){
+        /*quando premo avanti viene incrememntato l'indice e quindi cambia la mia immagine anche nell'html in quanto src="immagini[index]" */
+        next_f: function(){
             this.index++;
+            /*se il valore supera la lunghezza dell'array torna a 0*/
             if(this.index > this.immagini.length - 1) {
                 this.index = 0;
             }
         },
-        prev_f(){
-            console.log('ciao');
+        prev_f: function(){
             this.index--;
+            /*se il valore è inferiore a 0 torna a all'ultimo valore indice*/
             if(this.index < 0) {
                 this.index = this.immagini.length - 1;
             }
+        },
+        /*la funzione set_img viene chimata non appena premo un pallino, e ne viene passato il valore dell'indice che ci segnala la posizione */
+        set_img: function(indice){
+            console.log(indice);
+            /*per cambiare immagine non ci resta che settare il suo indice  uguale al valore(indice) dell'pallino cliccato*/
+            this.index = indice;
         }
-    }
+    },
+    created: function() {
+		setInterval(function() {
+        console.log('avanti');
+        this.next_f;
+        }, 5000);
+	}
+
 })
-
-/*Timer*/
-setInterval(myTimer, 3000);
-
-function myTimer() {
-    next_f();
-}
